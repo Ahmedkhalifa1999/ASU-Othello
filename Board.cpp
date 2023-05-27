75d7fe63e2a186e2bd33f8be028ceb72a7c78f69
Ahmedkhalifa1999 <ahmedkhalifa1999@gmail.com>
2023-05-27 14:47:02 +0300

    Board interfaces and function signatures

--- Board.cpp
+++ Board.cpp
@@ -1,7 +1,21 @@
 #include "Board.h"
 
-Board::Board(QObject *parent)
-    : QObject{parent}
+Board::Board()
+{
+
+}
+
+std::vector<Move> Board::getValidMoves(PlayerColor player)
+{
+
+}
+
+const std::vector<std::vector<BoardSquareState> > &Board::getBoard() const
+{
+
+}
+
+bool Board::doMove(Move move)
 {
 
 }
--- Board.h
+++ Board.h
@@ -3,13 +3,54 @@
 
 #include <QObject>
 
-class Board : public QObject
-{
-    Q_OBJECT
-public:
-    explicit Board(QObject *parent = nullptr);
+typedef enum {
+    EMPTY_SQUARE,
+    BLACK_DISK,
+    WHITE_DISK
+} BoardSquareState;
 
-signals:
+typedef enum {
+    BLACK_PLAYER,
+    WHITE_PLAYER
+} PlayerColor;
+
+typedef struct {
+    unsigned int x;
+    unsigned int y;
+} Move;
+
+class Board
+{
+    /**
+     * @brief 8-by-8 matrix reprenting the board
+     */
+    std::vector<std::vector<BoardSquareState>> board;
+public:
+
+    /**
+     * @brief Board Constructur; constructs the starting board with 4 disks in the middle (2 White and 2 Black)
+     */
+    Board();
+
+    /**
+     * @brief calculates the valid moves that the given player can do
+     * @param player: The color of the player whose valid moves are required
+     * @return a vector of valid moves the given polayer can choose, an empty vector means no legal moves are available
+     */
+    std::vector<Move> getValidMoves(PlayerColor player);
+
+    /**
+     * @brief allows access to the board state
+     * @return returns a constant reference to the 2D vector represnting the board
+     */
+    const std::vector<std::vector<BoardSquareState>>& getBoard() const;
+
+    /**
+     * @brief Mutates the board state according to passed move, if move is invalid, nothing happens
+     * @param move: Move choosen from valid moves
+     * @return return false if the passed move is invalid and true if it is
+     */
+    bool doMove(Move move);
 
 };
 
