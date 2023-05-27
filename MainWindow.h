741b60df7dfc600ca13ca4b8d76b2018d9397210
Ahmedkhalifa1999 <ahmedkhalifa1999@gmail.com>
2023-05-27 15:21:34 +0300

    Added initial interfaces to GUI

--- MainWindow.h
+++ MainWindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QThread>
+
+#include <AI.h>
+#include <Board.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -11,11 +15,21 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
+    Board mainBoard;
+
+    QThread AI_Thread;
+
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private:
     Ui::MainWindow *ui;
+
+public slots:
+    void nextMoveComputed(Move move);
+
+signals:
+    void computeNextMove();
 };
 #endif // MAINWINDOW_H
