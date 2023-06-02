6191206daf087eb7e6eacfbfbcae0d0d46396841
AmrAmin761 <amroshawky761@gmail.com>
2023-06-03 01:43:06 +0300

    Added files minmax and Node definition

---
+++ Algortithm/Node.hpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <Board.h>
+using namespace std;
+class Node{
+    public:
+        Board State;
+        //bool isRoot;
+        PlayerColor color;
+        vector<Node*> children;
+        int alpha;
+        int beta;
+        double Score = 0;
+
+        Node(Board State,PlayerColor color,int alpha,int beta,double Score);
+}; 
---
+++ Algortithm/minmax.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include "Board.h"
+#include "Node.hpp"
+#include "AI.h"
+#define ScoreStub 30;
+using namespace std;
+
+
+void treeConstruct(Node* currentNode,int depth){
+	if(depth == 0)
+		return; 
+	vector<Move> validMoves = currentNode->State.getValidMoves(currentNode->color);
+	
+	for(auto Move: validMoves){
+		Board StateCpy = currentNode->State;
+		StateCpy.doMove(Move,currentNode->color);
+		currentNode->children.push_back(new Node(StateCpy,currentNode->color,0,0,ScoreStub));
+		treeConstruct(currentNode->children.back(),depth - 1);
+	}
+}
+
+Move minimax(Node currentNode,bool Max,int alpha,int beta,int depth){
+	Move move;
+	if(depth == 0)
+		return move;
+	
+	if(Max)
+	{
+		
+		
+	}
+	else
+	{
+
+	}
+
+
+}
