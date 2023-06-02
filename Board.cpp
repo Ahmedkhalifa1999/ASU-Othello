84657b0c47040bdb1250e6946e9ed14c489cd95b
Ahmed Khalifa <ahmedkhalifa1999@gmail.com>
2023-06-02 21:34:28 +0300

    getBoard implementation

--- Board.cpp
+++ Board.cpp
@@ -10,9 +10,9 @@ std::vector<Move> Board::getValidMoves(PlayerColor player)
 
 }
 
-const std::vector<std::vector<BoardSquareState> > &Board::getBoard() const
+const std::vector<std::vector<BoardSquareState>> &Board::getBoard() const
 {
-
+    return board;
 }
 
 bool Board::doMove(Move move)
