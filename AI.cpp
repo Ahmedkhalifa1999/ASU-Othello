a5db2773af6a5ca2277c21a42b02b7a1e64381eb
AyaMohamed258 <84997395+AyaMohamed258@users.noreply.github.com>
2023-06-04 00:43:08 +0300

    Update AI.cpp
    1)Corners Captured: The function should work correctly to count the number of corners occupied by the player and return a score reflecting the player's advantage in occupying the corners.

--- AI.cpp
+++ AI.cpp
@@ -10,7 +10,29 @@ AI::AI(PlayerColor player, const Board& board)
 {
 
 }
+int AI::corners(Board& board, PlayerColor player) {
+    int corners[4][2] = {{0, 0}, {0, 7}, {7, 0}, {7, 7}};
+    int blackCorners = 0;
+    int whiteCorners = 0;
 
+    for (int i = 0; i < 4; i++) {
+        int row = corners[i][0];
+        int col = corners[i][1];
+        if (board[row][col] == BLACK_DISK) {
+              blackCorners++;
+        } else if (board[row][col] == WHITE_DISK) {
+        whiteCorners++;
+    }
+}
+    if (player == BLACK_PLAYER) {
+        return 100 * (blackCorners - whiteCorners)
+            / (blackCorners + whiteCorners + 1);
+    }
+    else {
+        return 100 * (whiteCorners - blackCorners)
+            / (blackCorners + whiteCorners + 1);
+    }
+}
 void AI::setDifficulty(Difficulty difficulty)
 {
 
