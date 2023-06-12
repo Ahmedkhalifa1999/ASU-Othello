1565edeadf2b60b4e80833fc1ccab64d1c9daed7
Ammaryasser265 <67715493+Ammaryasser265@users.noreply.github.com>
2023-06-12 19:16:01 +0300

    Update Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -1,6 +1,17 @@
-#include "Node.h"
+#include <vector>
+#include <Board.h>
+using namespace std;
+class Node{
+    public:
+        Board State;
+        int position;
+        //bool isRoot;
+        PlayerColor color;
+        vector<Node*> children;
+        Move moveDone;
+        int alpha = -INFINITY;
+        int beta = INFINITY;
+        double Score;
 
-Node::Node(Board State, PlayerColor color, int alpha, int beta, double Score)
-{
-
-}
+        Node(Board State,PlayerColor color,int alpha,int beta,double Score);
+}; 
